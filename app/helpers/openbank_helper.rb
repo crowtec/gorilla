aa1c29b460cffd9690e5ae81d07bef3559ab03f7
Meriem <melyamri@gmail.com>
2016-06-11 15:42:28 +0200

    fix auth

--- app/helpers/openbank_helper.rb
+++ app/helpers/openbank_helper.rb
@@ -8,9 +8,11 @@ module OpenbankHelper
     http.use_ssl = true
     http.verify_mode = OpenSSL::SSL::VERIFY_NONE
     request = Net::HTTP::Post.new(uri.path, {'Content-Type' =>'application/json'})
-    request["Authorization"] = "DirectLogin username='#{ENV['OPENBANK_USER']}', password='#{ENV['OPENBANK_PASSWORD']}', consumer_key='#{ENV['OPENBANK_CONSUMER_KEY']}'"
+    request["Authorization"] = "DirectLogin username=#{ENV['OPENBANK_USER']}, password=#{ENV['OPENBANK_PASSWORD']}, consumer_key=#{ENV['OPENBANK_CONSUMER_KEY']}"
 
     request.body = ""
+
+    p request.to_json
     response = http.request(request)
     p response.body
   end
