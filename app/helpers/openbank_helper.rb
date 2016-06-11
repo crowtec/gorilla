2c5605da500d4a54761949421cc277c623b63ebd
Meriem <melyamri@gmail.com>
2016-06-11 15:32:55 +0200

    auth openbank

---
+++ app/helpers/openbank_helper.rb
@@ -0,0 +1,17 @@
+module OpenbankHelper
+
+  def auth
+    base_url = 'https://apisandbox.openbankproject.com'
+    uri = URI.parse(base_url + "/my/logins/direct")
+
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    request = Net::HTTP::Post.new(uri.path, {'Content-Type' =>'application/json'})
+    request["Authorization"] = "DirectLogin username='#{ENV['OPENBANK_USER']}', password='#{ENV['OPENBANK_PASSWORD']}', consumer_key='#{ENV['OPENBANK_CONSUMER_KEY']}'"
+
+    request.body = ""
+    response = http.request(request)
+    p response.body
+  end
+end
